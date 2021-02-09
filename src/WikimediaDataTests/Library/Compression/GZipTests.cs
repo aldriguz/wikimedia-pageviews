fce171a3a0acd4bf2b6e3248c58d7122c8512035
Kevin Alama <alama1992@gmail.com>
2021-02-09 09:44:00 -0500

    add unit test project

---
+++ src/WikimediaDataTests/Library/Compression/GZipTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using WikimediaData.Compression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikimediaData.Compression.Tests
+{
+    public class GZipTests
+    {
+        GZip testInstance = new GZip();
+
+        [Fact()]
+        public void GetFileExtensionTest()
+        {
+            string value = testInstance.GetFileExtension();
+            Assert.Equal("gz", value);
+        }
+
+        [Fact()]
+        public void GetFileExtensionPatternTest()
+        {
+            string value = testInstance.GetFileExtension();
+            Assert.Equal("*.gz", value);
+        }
+    }
+}
